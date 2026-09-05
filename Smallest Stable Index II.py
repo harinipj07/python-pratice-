5f7e9d8332b371e253d45851dd865ce015d4ae91
harinipj07 <harinipj07@gmail.com>
2026-09-05 15:20:40 +0530

    Add files via upload

---
+++ Smallest Stable Index II.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def firstStableIndex(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        n = len(nums)
+        mn = [0] * n
+        mn[n-1] = nums[n-1]
+        for i in range(n-2, -1, -1):
+            mn[i] = min(nums[i], mn[i+1])
+        mxi = float('-inf')
+        for i in range(n):
+            mxi = max(mxi, nums[i])
+            if mxi - mn[i] <= k:
+                return i
+        return -1
